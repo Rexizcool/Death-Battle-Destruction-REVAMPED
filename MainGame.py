98ba9207845548ab47cefc32274ebed516ed6b2e
Rexizcool <s947182@student.knoxschools.org>
2026-04-07 20:30:33 +0000

    Idk lol

--- MainGame.py
+++ MainGame.py
@@ -1,11 +1,19 @@
 
-class Strike:
-    def __init__(self, strike_name, damage, self_damage, interrupt):
-        self.strike_name = strike_name
+class Move:
+    def __init__(self, name, damage, self_damage, interrupt, block):
+        self.name = name
         self.damage = damage
         self.self_damage = self_damage
         self.interrupt = interrupt
+        self.block = block
+
+
 
 class Character:
-    def __init__(self, name, strike, kick, dodge, parry, heal):
+    def __init__(self, name):
+        self.name = name
+
+
+class Knight(Character):
+    def __init__(self, name):
         self.name = name
