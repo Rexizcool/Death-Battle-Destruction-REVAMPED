37e4d6a2262bacc86ed24444c74ab14eaecde4b8
Rexizcool <s947182@student.knoxschools.org>
2026-04-09 19:03:00 +0000

    Began working on Knight

--- MainGame.py
+++ MainGame.py
@@ -6,14 +6,32 @@ class Move:
         self.self_damage = self_damage
         self.interrupt = interrupt
         self.block = block
+    def getdamage(self):
+        return self.damage
+    def getselfdamage(self):
+        return self.self_damage
+    def getinterrupt(self):
+        return self.interrupt
 
 
-
-class Character:
-    def __init__(self, name):
-        self.name = name
 
 
 class Knight(Character):
     def __init__(self, name):
         self.name = name
+    def strikeinfo(self):
+        damage=2
+        interrupt=True
+        movetype=strike
+        return damage, interrupt, movetype
+    def kickinfo(self):
+        damage=1
+        interrupt=False
+        movetype=kick
+        dodgedamage=3
+        return damage, interrupt, movetype, dodgedamage
+    def dodgeinfo(self):
+       dodging=True
+       interrupt=False
+
+
